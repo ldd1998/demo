35982a8e0c12190b61f952a1b8045d4198d0b79a
ldd1998 <1193804498@qq.com>
2023-05-04 19:37:03 +0800

    添加spring-boot-admin和srping-boot-admin-client

--- src/main/java/org/example/DemoApplication.java
+++ src/main/java/org/example/DemoApplication.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.oas.annotations.EnableOpenApi;
@@ -9,6 +10,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
  */
 @SpringBootApplication()
 @EnableOpenApi
+@EnableAdminServer
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class);
--- src/main/java/org/example/controller/DemoController.java
+++ src/main/java/org/example/controller/DemoController.java
@@ -1,13 +1,12 @@
 package org.example.controller;
 
-import org.example.mapper.UserMapper;
 import org.example.entity.User;
+import org.example.mapper.UserMapper;
 import org.example.service.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
 import java.util.List;
 
 /**
