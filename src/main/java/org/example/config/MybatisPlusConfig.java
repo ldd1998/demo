b151b8fb2f8de9fe3448897938b97166ebe472f4
ldd1998 <1193804498@qq.com>
2023-05-16 20:01:36 +0800

    MybatisPlusConfig 去除mapperscan也能使用

--- src/main/java/org/example/config/MybatisPlusConfig.java
+++ src/main/java/org/example/config/MybatisPlusConfig.java
@@ -1,9 +1,6 @@
 package org.example.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
  * @author ldd
  */
 @Configuration
-@MapperScan("com.baomidou.cloud.service.*.mapper*")
 public class MybatisPlusConfig {
 	//旧版
     @Bean
