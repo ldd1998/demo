ba37aa828dc066256829ddfa882b8c461f65a2df
ldd1998 <1193804498@qq.com>
2023-05-26 23:07:20 +0800

    修改目录结构

--- src/main/java/org/example/common/task/monitor/MonitorDbTask.java
+++ src/main/java/org/example/common/task/monitor/MonitorDbTask.java
@@ -1,4 +1,4 @@
-package org.example.common.task;
+package org.example.common.task.monitor;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.example.mapper.UserMapper;
