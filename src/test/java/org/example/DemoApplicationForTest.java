65545c8aa9e359ccf978578174792652849848d7
ldd1998 <1193804498@qq.com>
2023-05-26 23:10:32 +0800

    增加测试专用启动类

---
+++ src/test/java/org/example/DemoApplicationForTest.java
@@ -0,0 +1,17 @@
+package org.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ldd
+ * 只应用于测试启动项目，关闭了不必要的注解
+ */
+@SpringBootApplication
+//@EnableOpenApi
+//@EnableAdminServer
+//@EnableScheduling
+public class DemoApplicationForTest {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplicationForTest.class);
+    }
+}
