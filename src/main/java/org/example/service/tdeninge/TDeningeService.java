778e164445c8b93da503da032d203886699be82f
ldd1998 <1193804498@qq.com>
2023-06-24 09:54:19 +0800

    测试tdengine插入性能，不理想

--- src/main/java/org/example/service/tdeninge/TDeningeService.java
+++ src/main/java/org/example/service/tdeninge/TDeningeService.java
@@ -29,6 +29,54 @@ public class TDeningeService {
             stbMapper.insertByTable(new Stb(),tableName);
         }
     }
+    public void stbMapperInsert02(int count){
+        for (int i = 0; i < count; i++) {
+            String tableName = "t"+i%100;
+            stbMapper.insertByTable(new Stb(),tableName);
+        }
+    }
+    public void stbMapperInsert03(int count) throws ClassNotFoundException, SQLException {
+        Class.forName("com.taosdata.jdbc.rs.RestfulDriver");
+        String jdbcUrl = "jdbc:TAOS-RS://192.168.1.107:6041/test?user=root&password=taosdata";
+        Connection conn = DriverManager.getConnection(jdbcUrl);
+        Statement statement = conn.createStatement();
+        for (int i = 0; i < count; i++) {
+            statement.execute("insert into t2 using stb tags(1,'tag2') values(now,2);");
+        }
+    }
+    public void stbMapperInsert04(int count) throws ClassNotFoundException, SQLException {
+        Class.forName("com.taosdata.jdbc.rs.RestfulDriver");
+        String jdbcUrl = "jdbc:TAOS-RS://192.168.1.107:6041/test?user=root&password=taosdata";
+        Connection conn = DriverManager.getConnection(jdbcUrl);
+        Statement statement = conn.createStatement();
+        StringBuffer sql = new StringBuffer();
+        sql.append("insert into t2 using stb tags(1,'tag2') values");
+        for (int i = 0; i < count; i++) {
+            sql.append("(now,2)");
+        }
+        statement.execute(sql.toString());
+    }
+    public void stbMapperInsert05(int count) throws ClassNotFoundException, SQLException {
+        Class.forName("com.taosdata.jdbc.rs.RestfulDriver");
+        String jdbcUrl = "jdbc:TAOS-RS://192.168.1.107:6041/test?user=root&password=taosdata";
+        Connection conn = DriverManager.getConnection(jdbcUrl);
+        Statement statement = conn.createStatement();
+        for (int i = 0; i < count; i++) {
+            String tableName = "t"+i%100;
+            statement.addBatch("insert into "+"t"+i%100+" using stb tags(1,'tag2') values (now,2)");
+        }
+        statement.executeBatch();
+    }
+    public void stbMapperInsert06(int count) throws ClassNotFoundException, SQLException {
+        Class.forName("com.taosdata.jdbc.rs.RestfulDriver");
+        String jdbcUrl = "jdbc:TAOS-RS://192.168.1.107:6041/test?user=root&password=taosdata";
+        Connection conn = DriverManager.getConnection(jdbcUrl);
+        Statement statement = conn.createStatement();
+        for (int i = 0; i < count; i++) {
+            statement.addBatch("insert into "+"t"+i%1000+" using stb tags(1,'tag2') values (now,2)");
+        }
+        statement.executeBatch();
+    }
     public void stbMapperSelect(){
         String tableName = "t1";
         List<Stb> stbs = stbMapper.selectByTable(tableName);
--- src/test/java/org/example/service/tdeninge/TDeningeServiceTest.java
+++ src/test/java/org/example/service/tdeninge/TDeningeServiceTest.java
@@ -5,20 +5,83 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DemoApplicationForTest.class,webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import java.sql.SQLException;
+
+@SpringBootTest(classes = DemoApplicationForTest.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class TDeningeServiceTest {
     @Autowired
     TDeningeService tDeningeService;
+
     @Test
-    public void Test(){
+    public void Test() {
         tDeningeService.stbMapperSelectList();
     }
+
+    /**
+     * 插入100000条数据用时109秒
+     * 917/s，好慢
+     */
     @Test
-    public void Test1(){
-        tDeningeService.stbMapperInsert(100000);
+    public void TestInsert01() {
+        tDeningeService.stbMapperInsert(10000);
     }
+
+    /**
+     * 分别插入到100张子表，共10000条数据用时23秒
+     * 430/s
+     */
     @Test
-    public void Test2(){
+    public void TestInsert02() {
+        tDeningeService.stbMapperInsert02(10000);
+    }
+
+    /**
+     * 插入10000条数据，14秒
+     * 714/s
+     *
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    @Test
+    public void TestInsert03() throws SQLException, ClassNotFoundException {
+        tDeningeService.stbMapperInsert03(10000);
+    }
+
+    /**
+     * 100000条，317ms
+     * 27w/秒，有点快了，但是发现数据库中只有几十条，原来时间戳不能重复。
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    @Test
+    public void TestInsert04() throws SQLException, ClassNotFoundException {
+        tDeningeService.stbMapperInsert04(100000);
+    }
+
+    /**
+     * 100000条耗时114秒
+     *
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    @Test
+    public void stbMapperInsert05() throws SQLException, ClassNotFoundException {
+        tDeningeService.stbMapperInsert05(100000);
+    }
+
+    /**
+     * 分成1000张表也是100000条耗时114秒
+     *
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
+    @Test
+    public void stbMapperInsert06() throws SQLException, ClassNotFoundException {
+        tDeningeService.stbMapperInsert06(100000);
+    }
+
+    @Test
+    public void Test2() {
         tDeningeService.stbMapperSelect();
     }
 }
