86a68a5fbef8ac6d971109a901e06adb6a7caf9c
ldd1998 <1193804498@qq.com>
2023-09-25 21:32:11 +0800

    nginx转发post请求测试controllerd的bug

--- src/main/java/org/example/controller/NginxController.java
+++ src/main/java/org/example/controller/NginxController.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * 该类模拟nginx转发post请求问题// TODO
  * 未完成完整模拟
  */
-@RestController("/post")
+@RestController()
 public class NginxController {
     /**
      * 普通的get请求，可以通过nginx
@@ -32,26 +32,26 @@ public class NginxController {
     /**
      * 带restfull参数请求，没有问题
      */
-    @PostMapping("/post2/{uri}")
-    public String post2(@PathVariable String uri){
-        return "请求成功:"+uri;
+    @PostMapping("/post2/")
+    public String post2(){
+        return "请求成功:";
     }
     /**
      * 带restfull参数请求的get请求
      * 最终发现为ResponseEntity的问题
      */
-    @PostMapping("{uri}/**")
-    public ResponseEntity post3(@PathVariable String uri, @RequestBody Map<String,String> body){
-        ResponseEntity<String> forEntity = new ResponseEntity(HttpStatus.OK);
+    @PostMapping("/post3")
+    public ResponseEntity<String> post3(){
+        ResponseEntity<String> forEntity = new ResponseEntity<>(HttpStatus.OK);
         return forEntity;
     }
 
     /**
      * 最终解决办法
      */
-    @PostMapping("{uri}/**")
-    public Object post4(@PathVariable String uri, @RequestBody Map<String,String> body){
-        ResponseEntity<String> forEntity = new ResponseEntity(HttpStatus.OK);
+    @PostMapping("/post4")
+    public Object post4(){
+        ResponseEntity<String> forEntity = new ResponseEntity<>("{\"a\":\"b\"}",HttpStatus.OK);
         return JSONObject.parseObject(forEntity.getBody());
     }
 }
